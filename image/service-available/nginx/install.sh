27faf2d5916581e08632f9d31b544d3c99409965
Bertrand Gouny <bgouny@hi-media.com>
2015-10-19 17:30:31 +0200

    update ssl config

--- image/service-available/nginx/install.sh
+++ image/service-available/nginx/install.sh
@@ -5,6 +5,7 @@ LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommen
 
 # add configs
 ln -sf /container/service-available/nginx/assets/nginx.conf /etc/nginx/nginx.conf
+ln -s /container/service-available/nginx/assets/server-partial-ssl.conf /etc/nginx/server-partial-ssl.conf
 ln -s /container/service-available/nginx/assets/conf.d/* /etc/nginx/conf.d
 
 # create sites-enabled directory to keep debian style
