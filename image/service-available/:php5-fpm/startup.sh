5da11f40e57d4baf94ab8f1e6fa0153bab229eaf
Bertrand Gouny <bgouny@hipay.com>
2016-01-18 15:16:30 +0100

    change msg log levels

--- image/service-available/:php5-fpm/startup.sh
+++ image/service-available/:php5-fpm/startup.sh
@@ -13,7 +13,7 @@ fi
 # apache2 is installed
 if [ -d "/etc/apache2" ]; then
   ln -sf ${CONTAINER_SERVICE_DIR}/:php5-fpm/assets/config/apache2/php5-fpm.conf /etc/apache2/conf-available/php5-fpm.conf
-  a2enconf php5-fpm | log-helper info
+  a2enconf php5-fpm | log-helper debug
 fi
 
 exit 0
